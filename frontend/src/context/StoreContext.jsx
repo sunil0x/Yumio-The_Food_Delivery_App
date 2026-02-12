05e33e3a6398b192676f1c4ea7760a66154f83c5
Shishir Neupane <shishirapk@gmail.com>
2026-02-12 23:52:38 +0545

    Update API URL to localhost for development

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -9,7 +9,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "https://yumio-backend-zs2j.onrender.com";
+    const url = "http://localhost:4000";
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
